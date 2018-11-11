ea1bdc73b9f44b8ac71072006f5862451166de14
Fellipe <gurgelfellipe@gmail.com>
2018-11-11 10:33:04 -0200

    Initial spring project files upload

---
+++ src/main/java/br/com/zup/rest/springxyinc/SpringXyIncApplication.java
@@ -0,0 +1,12 @@
+package br.com.zup.rest.springxyinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXyIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXyIncApplication.class, args);
+	}
+}
