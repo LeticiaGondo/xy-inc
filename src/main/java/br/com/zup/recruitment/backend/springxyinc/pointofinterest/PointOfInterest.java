b5c83d13987c63ab53821ec6bb5408e976cba868
Fellipe <gurgelfellipe@gmail.com>
2018-11-11 16:39:23 -0200

    Added the class "Point of Interest" as entity to the H2 testing db along with some initial data to this table (POINT_OF_INTEREST)

---
+++ src/main/java/br/com/zup/recruitment/backend/springxyinc/pointofinterest/PointOfInterest.java
@@ -0,0 +1,71 @@
+package br.com.zup.recruitment.backend.springxyinc.pointofinterest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+/**
+ * Created by Fellipe G on 11/11/2018.
+ */
+@Entity
+public class PointOfInterest {
+
+    @Id
+    @GeneratedValue
+    private Long id; // point of interest's id
+
+    @NotNull
+    @NotBlank
+    private String name; // point of interest's name
+
+    @PositiveOrZero
+    private int xCoordinate; // latitude
+
+    @PositiveOrZero
+    private  int yCoordinate; // longitude
+
+    public PointOfInterest() {
+        super();
+    }
+
+    public PointOfInterest(@NotNull @NotBlank String name, @PositiveOrZero int xCoordinate, @PositiveOrZero int yCoordinate) {
+        this.name = name;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+}
